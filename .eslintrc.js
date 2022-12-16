562e413c31ebfa5670101fbc461d12bce2bd7e7a
Anthony Sotomayor <anthonysv@gmail.com>
2022-12-16 18:30:28 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: 'standard',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prefer-const': 'off'
+  }
+}
